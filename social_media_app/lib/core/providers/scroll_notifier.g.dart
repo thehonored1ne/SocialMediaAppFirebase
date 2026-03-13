f8781173e98930c1ef8c787ea06d8becbbf12a49
dion <dion.areglo1234@gmail.com>
2026-03-13 11:12:39 +0800

    add files to error and providers

---
+++ social_media_app/lib/core/providers/scroll_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'scroll_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$homeScrollNotifierHash() =>
+    r'aafebc76ba02b401849ad90f76588b6186da2c31';
+
+/// See also [HomeScrollNotifier].
+@ProviderFor(HomeScrollNotifier)
+final homeScrollNotifierProvider =
+    AutoDisposeNotifierProvider<HomeScrollNotifier, int>.internal(
+      HomeScrollNotifier.new,
+      name: r'homeScrollNotifierProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$homeScrollNotifierHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$HomeScrollNotifier = AutoDisposeNotifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
