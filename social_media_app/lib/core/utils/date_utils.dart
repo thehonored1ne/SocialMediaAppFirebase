74edcefefc0844ad0f4f2196ef9d618995d5672a
dion <dion.areglo1234@gmail.com>
2026-03-13 11:52:02 +0800

    fix Fix logic errors in AppDateUtils

--- social_media_app/lib/core/utils/date_utils.dart
+++ social_media_app/lib/core/utils/date_utils.dart
@@ -3,13 +3,13 @@ import 'package:intl/intl.dart';
 class AppDateUtils {
   static String formatTimeAgo(DateTime? date) {
     if (date == null) return '';
-    final now = DateTime.now().toUtc(); 
+    final now = DateTime.now();
     final diff = now.difference(date);
 
-    if (diff.inDays < 7) { 
+    if (diff.inDays > 7) {
       return DateFormat('MMM d, yyyy').format(date);
     } else if (diff.inDays > 0) {
-      return '${diff.inDays}dago'; 
+      return '${diff.inDays}d ago';
     } else if (diff.inHours > 0) {
       return '${diff.inHours}h ago';
     } else if (diff.inMinutes > 0) {
