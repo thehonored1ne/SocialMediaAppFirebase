45ca52e4713ae5d9877ae3e3ef924da1dcd85016
dion <dion.areglo1234@gmail.com>
2026-03-13 12:53:10 +0800

    add 3 files to core features of the app

---
+++ social_media_app/lib/core/router/app_routes.dart
@@ -0,0 +1,23 @@
+class AppRoutes {
+  static const String login = 'login';
+  static const String register = 'register';
+  static const String feed = 'feed';
+  static const String profile = 'profile';
+  static const String community = 'community';
+  static const String chat = 'chat';
+  static const String notifications = 'notifications';
+  static const String messages = 'messages';
+  static const String editPost = 'editPost';
+  static const String editCommunity = 'editCommunity';
+  static const String postDetail = 'postDetail';
+  static const String search = 'search';
+  static const String reels = 'reels';
+  static const String createPost = 'createPost';
+  static const String createStory = 'createStory';
+  static const String createReel = 'createReel';
+  static const String createCommunity = 'createCommunity';
+  static const String storyView = 'storyView';
+  static const String fullContent = 'fullContent';
+  static const String editProfile = 'editProfile';
+  static const String settings = 'settings';
+}
