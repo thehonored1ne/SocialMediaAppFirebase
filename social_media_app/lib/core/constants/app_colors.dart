9d9933bffd4c9b319c4dbee48930e1b66ffe1149
dion <dion.areglo1234@gmail.com>
2026-03-13 10:15:20 +0800

    add 2 constants

---
+++ social_media_app/lib/core/constants/app_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF0A0A0A);
+  static const Color surface = Color(0xFF141414);
+  static const Color primary = Color(0xFFE94057);
+  static const Color secondary = Color(0xFF8A2387);
+  static const Color accent = Color(0xFFF27121);
+  static const Color error = Color(0xFFFF4B4B);
+
+  static const Color textPrimary = Colors.white;
+  static const Color textSecondary = Colors.white54;
+  static const Color textTertiary = Colors.white24;
+
+  // Code Snippet Colors
+  static const Color codeBackground = Color(0xFF1E1E1E);
+  static const Color codeHeader = Color(0xFF2D2D2D);
+
+  static const List<Color> primaryGradient = [
+    Color(0xFF8A2387),
+    Color(0xFFE94057),
+    Color(0xFFF27121),
+  ];
+  static const Color onlineStatus = Color(0xFF4CAF50);
+}
