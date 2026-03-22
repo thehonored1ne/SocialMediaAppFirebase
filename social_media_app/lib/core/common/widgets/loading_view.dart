09bcfe13bcb3e72905ec6291ba6eea91a9247560
hazel <hazelannconvicto@gmail.com>
2026-03-22 21:27:34 +0800

    add common folder to core

---
+++ social_media_app/lib/core/common/widgets/loading_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoadingView extends StatelessWidget {
+  final String? message;
+  const LoadingView({super.key, this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CircularProgressIndicator(color: Colors.blueAccent),
+          if (message != null) ...[
+            const SizedBox(height: 16),
+            Text(
+              message!,
+              style: const TextStyle(color: Colors.white54, fontSize: 14),
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+}
